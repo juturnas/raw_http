3cc8cc227f9e64630194f3421d138d6062d903a3
Tyler Bender <juturnas@gmail.com>
2023-01-12 12:13:40 -0800

    fix paths

--- src/raw_http.cr
+++ src/raw_http.cr
@@ -1,7 +1,7 @@
-require "./raw_http/header"
-require "./raw_http/body"
-require "./raw_http/message"
-require "./raw_http/roundtrip"
+require "./header"
+require "./body"
+require "./message"
+require "./roundtrip"
 
 module RawHTTP
   VERSION = "0.1.0"
